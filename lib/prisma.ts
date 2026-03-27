c94a47732e7019a6f6d900c481bb955b2d861b18
amboseshem <amboseshem@gmail.com>
2026-03-27 17:14:57 +0300

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,27 +1,15 @@
-let prisma: any;
+import { PrismaClient } from "@prisma/client";
 
-if (process.env.NODE_ENV === "production") {
-  // In build time, avoid crashing
-  prisma = {
-    user: {},
-    memberProfile: {},
-    branch: {},
-    contribution: {},
-    commission: {},
-    project: {},
-  };
-} else {
-  const { PrismaClient } = require("@prisma/client");
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-  const globalForPrisma = globalThis as any;
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
 
-  prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient();
-
-  if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
-  }
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-export { prisma };
