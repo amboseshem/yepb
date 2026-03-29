db060ae41093cd0985b81fe7c13b830b189fbd50
Ambrose Shem <amboseshem@gmail.com>
2026-03-29 13:32:48 +0300

    Fix TrainerDashboard case issue

--- app/dashboard/roles/trainerDashboard.tsx
+++ app/dashboard/roles/trainerDashboard.tsx
@@ -1,6 +1,6 @@
 export default function TrainerDashboard() {
   return (
-    <div>
+    <div className="p-6">
       <h1 className="text-2xl font-bold">Trainer Dashboard</h1>
       <p className="mt-3">Welcome Trainer 👨‍🏫</p>
     </div>
